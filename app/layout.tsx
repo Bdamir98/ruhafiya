fdd1deb59316eeb1d72ee0ca110eef4389829bac
Bdami98 <amirshohel870@gmail.com>
2025-08-22 23:29:49 +0600

    tag manager update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,38 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="bn">
       <head>
         <meta name="facebook-domain-verification" content="dcyvqi2kznnlrpp6r7dg6efm47ymls" />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(
+              function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              }
+            )(window,document,'script','dataLayer','GTM-TH6FQJD8');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${hindSiliguri.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TH6FQJD8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {process.env.NEXT_PUBLIC_FB_PIXEL_ID && (
           <>
             <Script
